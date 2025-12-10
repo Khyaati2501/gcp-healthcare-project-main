eb3dac75c1480e9493483d1fa27b57bfa68cbffa
Khyaati Bhumireddy <khyaati.52001@gmail.com>
2025-12-09 21:05:13 -0500

    removung autodetect

--- data/BQ/bronze.sql
+++ data/BQ/bronze.sql
@@ -5,35 +5,35 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.depart
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-A/departments/*.json'],
-  autodetect = TRUE
+  
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.encounters_HA` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-A/encounters/*.json'],
-  autodetect = TRUE
+ 
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.patients_HA` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-A/patients/*.json'],
-  autodetect = TRUE
+  
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.providers_HA` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-A/providers/*.json'],
-  autodetect = TRUE
+  
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.transactions_HA` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-A/transactions/*.json'],
-  autodetect = TRUE
+  
 );
 
 ---------------------------------------------------------------------------------------------------------------------------
@@ -42,35 +42,35 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.depart
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-B/departments/*.json'],
-  autodetect = TRUE
+  
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.encounters_HB` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-B/encounters/*.json'],
-  autodetect = TRUE
+  
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.patients_HB` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-B/patients/*.json'],
-  autodetect = TRUE
+ 
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.providers_HB` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-B/providers/*.json'],
-  autodetect = TRUE
+  
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-healthcare-479816.bronze_dataset.transactions_HB` 
 OPTIONS (
   format = 'JSON',
   uris = ['gs://healthcare-11-30/landing/hospital-B/transactions/*.json'],
-  autodetect = TRUE
+  
 );
 
 ---------------------------------------------------------------------------------------------------------------------------
